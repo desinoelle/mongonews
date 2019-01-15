5f8161729e16d697c83146c23abbe3c5343fe268
Des <desinoelle@gmail.com>
2019-01-15 04:42:10 -0500

    server stuff.

---
+++ server.js
@@ -0,0 +1,5 @@
+// For connecting mongo database to mongoose
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI);
